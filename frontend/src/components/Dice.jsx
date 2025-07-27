bba6b7007248c42b1afd99a79422c638db709aff
Alec Southward <alecsouthward@gmail.com>
2025-07-27 23:54:54 +0200

    Improve Dice component to allow consecutive 1/20 tolls and remove roll type header

--- frontend/src/components/Dice.jsx
+++ frontend/src/components/Dice.jsx
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 
 import { FaArrowsSpin } from "react-icons/fa6";
 
-export default function Dice({ totalRoll = 20, rollType = "Dexterity", additions = [1, 3, 5] }) {
+export default function Dice({ totalRoll = 20, additions = [1, 3, 5] }) {
     const [roll, setRoll] = useState(undefined);
 
     const handleRoll = () => {
@@ -13,14 +13,14 @@ export default function Dice({ totalRoll = 20, rollType = "Dexterity", additions
 
         do {
             newRoll = Math.ceil(Math.random() * totalRoll);
-        } while (newRoll === roll);
+        } while (newRoll === roll && newRoll !== 20 && newRoll !== 1);
 
         setRoll(newRoll);
     };
 
     return (
         <div className="dice-menu-container">
-            <h1>Roll for {rollType}</h1>
+            <h1>Roll the Dice</h1>
             <hr className="title-separator"></hr>
 
             <div className="dice-container">
@@ -48,6 +48,5 @@ export default function Dice({ totalRoll = 20, rollType = "Dexterity", additions
 
 Dice.propTypes = {
     totalRoll: PropTypes.number,
-    rollType: PropTypes.string,
     additions: PropTypes.arrayOf(PropTypes.number),
 };
