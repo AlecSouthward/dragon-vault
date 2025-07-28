36b0340626fcc492bbe915b745426c21408c46d0
Alec Southward <alecsouthward@gmail.com>
2025-07-28 20:18:11 +0200

    Implement Item viewing Window Character screen

--- frontend/src/pages/Character.jsx
+++ frontend/src/pages/Character.jsx
@@ -3,8 +3,10 @@ import "./css/Character.css";
 import PropTypes from "prop-types";
 import { useState } from "react";
 
-import { FaPlus, FaRegEye } from "react-icons/fa";
-import { FaEye } from "react-icons/fa6";
+import { LuEye, LuEyeClosed } from "react-icons/lu";
+import { FaPlus } from "react-icons/fa";
+
+import ITEMS from "../constants/Items";
 
 const StatRow = ({ statName, statValue, canIncrease = false }) => (
     <div className="stat-row-container">
@@ -21,46 +23,73 @@ const StatRow = ({ statName, statValue, canIncrease = false }) => (
     </div>
 );
 
-const ItemRow = ({ itemName, itemSelected = false }) => (
-    <div className={"item-row-container" + (itemSelected ? " selected" : "")}>
+const ItemRow = ({ itemName, handleClick, itemSelected = false }) => (
+    <button className={"item-row-container" + (itemSelected ? " selected" : "")} onClick={handleClick}>
         <p className="item-row-name">{itemName}</p>
-        {itemSelected ? <FaEye /> : <FaRegEye />}
+        {itemSelected ? <LuEye /> : <LuEyeClosed />}
+    </button>
+);
+
+const CharacterWindow = ({ characterName, characterDescription }) => (
+    <div className="character-window-container">
+        <h1 className="character-window-title">{characterName}</h1>
+        <p className="character-window-description">{characterDescription}</p>
+    </div>
+);
+
+const ItemWindow = ({ itemName, itemDescription, hidden }) => (
+    <div className={"item-window-container" + (hidden ? " hidden" : "")}>
+        <h1 className="item-window-title">{itemName}</h1>
+        <p className="item-window-description">{itemDescription}</p>
     </div>
 );
 
 export default function Character({ userId }) {
     const [canIncreaseStats, setCanIncreaseStats] = useState(true);
+    const [selectedItem, setSelectedItem] = useState(undefined);
 
     return (
         <div className="character-page-container">
-            <h1>(character_name) SHEET</h1>
-            <hr />
+            <CharacterWindow />
 
-            <div className="stats-container">
-                <h1>STATISTICS</h1>
+            <div className="sheet-container">
+                <h1>(character_name) Sheet</h1>
+                <hr />
 
-                <StatRow statName="Strength" statValue={1} canIncrease={canIncreaseStats} />
-                <StatRow statName="Dexterity" statValue={2} canIncrease={canIncreaseStats} />
-                <StatRow statName="Charisma" statValue={3} canIncrease={canIncreaseStats} />
-                <StatRow statName="Constitution" statValue={4} canIncrease={canIncreaseStats} />
-                <StatRow statName="Intelligence" statValue={5} canIncrease={canIncreaseStats} />
-            </div>
+                <div className="stats-container">
+                    <h1>Statistics</h1>
 
-            <div className="items-container">
-                <h1>ITEMS</h1>
+                    <StatRow statName="Strength" statValue={1} canIncrease={canIncreaseStats} />
+                    <StatRow statName="Dexterity" statValue={2} canIncrease={canIncreaseStats} />
+                    <StatRow statName="Charisma" statValue={3} canIncrease={canIncreaseStats} />
+                    <StatRow statName="Constitution" statValue={4} canIncrease={canIncreaseStats} />
+                    <StatRow statName="Intelligence" statValue={5} canIncrease={canIncreaseStats} />
+                </div>
 
-                <div className="items-list-container">
-                    <ItemRow itemName="Sword" itemSelected={true} />
-                    <ItemRow itemName="Armor" />
-                    <ItemRow itemName="Knife" />
-                    <ItemRow itemName="Dagger" />
-                    <ItemRow itemName="Artifact" />
-                    <ItemRow itemName="Artifact" />
-                    <ItemRow itemName="Artifact" />
-                    <ItemRow itemName="Artifact" />
-                    <ItemRow itemName="Artifact" />
+                <div className="items-container">
+                    <h1>Items</h1>
+
+                    <div className="items-list-container">
+                        {Object.values(ITEMS).map((item) => (
+                            <ItemRow
+                                key={item.name}
+                                itemName={item.name}
+                                itemSelected={selectedItem === item}
+                                handleClick={() => setSelectedItem(prev => {
+                                    if (prev === item) return null;
+                                    else return item;
+                                })}
+                            />
+                        ))}
+                    </div>
                 </div>
             </div>
+
+            <ItemWindow
+                itemName={selectedItem?.name}
+                itemDescription={selectedItem?.description}
+                hidden={!selectedItem}
+            />
         </div>
     );
 }
@@ -69,6 +98,17 @@ Character.propTypes = {
     userId: PropTypes.string.isRequired
 };
 
+CharacterWindow.propTypes = {
+    characterName: PropTypes.string.isRequired,
+    characterDescription: PropTypes.string
+};
+
+ItemWindow.propTypes = {
+    itemName: PropTypes.string.isRequired,
+    itemDescription: PropTypes.string,
+    hidden: PropTypes.bool
+}
+
 StatRow.propTypes = {
     statName: PropTypes.string.isRequired,
     statValue: PropTypes.number.isRequired,
@@ -76,5 +116,7 @@ StatRow.propTypes = {
 };
 
 ItemRow.propTypes = {
-    itemName: PropTypes.string.isRequired
+    itemName: PropTypes.string.isRequired,
+    handleClick: PropTypes.func.isRequired,
+    itemSelected: PropTypes.bool
 };
