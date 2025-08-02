4d5b3c4a60b8137f9391d2005bb8d1d5aa04b113
Alec Southward <alecsouthward@gmail.com>
2025-08-02 21:16:00 +0200

    Put dockerfile files into their respective dirs and add the database init script

--- backend/src/routes/campaign.js
+++ backend/src/routes/campaign.js
@@ -4,7 +4,7 @@ export default async function (fastify) {
   fastify.get("/retrieve-for-user", { preHandler: [fastify.authenticate] }, async (req, reply) => {
     const { id: userId } = req.user;
 
-    const databaseResult = await database.query("SELECT id, name FROM campaigns WHERE owner_id = $1", [userId]);
+    const databaseResult = await database.query("SELECT id, name FROM campaigns WHERE creator_user_id = $1", [userId]);
 
     reply.code(200).send({ campaigns: databaseResult.rows });
   });
@@ -13,7 +13,7 @@ export default async function (fastify) {
     const { name } = req.body;
     const { id: userId } = req.user;
 
-    await database.query("INSERT INTO campaigns (name, owner_id) VALUES ($1, $2)", [name, userId]);
+    await database.query("INSERT INTO campaigns (name, creator_user_id) VALUES ($1, $2)", [name, userId]);
 
     reply.code(200).send({ message: "Successfully created Campagin" });
   });
