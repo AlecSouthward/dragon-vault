531af3edeb37f70ee56e11cd83510184133c8eed
Alec Southward <alecsouthward@gmail.com>
2025-08-08 14:13:48 +0200

    Add custom Font and add a new icon

--- frontend-vite/src/App.tsx
+++ frontend-vite/src/App.tsx
@@ -1,9 +1,16 @@
 function App(): React.JSX.Element {
   return (
-    <>
-      <h1 className="a">Dragon Vault</h1>
-      <img className="w-32" src="/icon.png" alt="Dragon Vault Icon" />
-    </>
+    <div className="flex h-screen w-full flex-col bg-zinc-900">
+      <h1 className="font-primary text-4xl font-bold text-zinc-300 select-none">
+        Dragon Vault
+      </h1>
+
+      <img
+        className="pointer-events-none w-32 fill-zinc-300 select-none"
+        src="/icon.svg"
+        alt="Dragon Vault Icon"
+      />
+    </div>
   );
 }
 
