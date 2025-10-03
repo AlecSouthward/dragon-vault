5d6f67c3450a207dcb65c4151dcf86fd2c1dd1e8
Alec Southward <alecsouthward@gmail.com>
2025-10-03 08:04:30 +0200

    Change the database init script to match the database diagram

--- database/init.sql
+++ database/init.sql
@@ -1,78 +1,134 @@
-CREATE EXTENSION hstore;
+CREATE EXTENSION IF NOT EXISTS hstore;
+CREATE EXTENSION IF NOT EXISTS citext;
 
-CREATE TABLE users (
-    "id" UUID NOT NULL DEFAULT uuidv7() PRIMARY KEY,
-    "username" TEXT UNIQUE NOT NULL,
-    "password" TEXT NOT NULL,
-    "profile_picture" VARCHAR(255),
-    "is_admin" BOOLEAN NOT NULL DEFAULT FALSE
-);
-
-CREATE TABLE user_invites (
+CREATE TABLE user_account (
     "id" UUID NOT NULL DEFAULT uuidv7() PRIMARY KEY,
     "created_date" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    "used_by_user_id" UUID REFERENCES users (id)
+    "username" CITEXT UNIQUE NOT NULL,
+    "password" TEXT NOT NULL,
+    "profile_picture" TEXT,
+    "admin" BOOLEAN NOT NULL DEFAULT FALSE,
+    "deleted" BOOLEAN NOT NULL DEFAULT FALSE
 );
 
 INSERT INTO
-    users (username, password, is_admin)
+    user_account (username, password, admin)
 VALUES (
         'admin',
-        '$argon2id$v=19$m=131072,t=3,p=1$4AGYvLd91uwQUsrIc3JQSQ$ngHVPKjYZloTtV04NpZsxPXTR9DtolDGDWdOFz17onc', -- root (assuming that PASSWORD_PEPPER is 'passpepper')
+        '$argon2id$v=19$m=131072,t=3,p=1$4AGYvLd91uwQUsrIc3JQSQ$ngHVPKjYZloTtV04NpZsxPXTR9DtolDGDWdOFz17onc', -- root (assuming that PASSWORD_PEPPER is "passpepper")
         TRUE
     );
 
-CREATE TABLE "campaigns" (
+CREATE TABLE user_invite (
     "id" UUID NOT NULL DEFAULT uuidv7() PRIMARY KEY,
-    "name" VARCHAR(96) NOT NULL,
+    "created_date" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    "used_date" TIMESTAMPTZ,
+    "used_by_user_account_id" UUID REFERENCES user_account(id)
+);
+
+CREATE TABLE campaign (
+    "id" UUID NOT NULL DEFAULT uuidv7() PRIMARY KEY,
+    "creator_user_account_id" UUID NOT NULL REFERENCES user_account(id),
+    "created_date" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    "name" TEXT NOT NULL,
     "description" TEXT,
-    "created" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "creator_user_id" UUID NOT NULL REFERENCES users (id) ON DELETE CASCADE
+    "story" TEXT,
+    "icon" TEXT
 );
 
-CREATE TABLE "campaign_admins" (
+CREATE TABLE campaign_admins (
     "id" UUID NOT NULL DEFAULT uuidv7() PRIMARY KEY,
-    "user_id" UUID NOT NULL REFERENCES users (id) ON DELETE CASCADE,
-    "campaign_id" UUID NOT NULL REFERENCES campaigns (id) ON DELETE CASCADE
+    "user_account_id" UUID NOT NULL REFERENCES user_account(id),
+    "campaign_id" UUID NOT NULL REFERENCES campaign(id)
 );
 
-CREATE TABLE "characters" (
-  "id" UUID NOT NULL DEFAULT uuidv7() PRIMARY KEY,
-  "campaign_id" UUID NOT NULL REFERENCES campaigns(id) ON DELETE CASCADE,
-  "user_id" UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-  "name" VARCHAR(64),
-  "description" TEXT,
-  "class" VARCHAR(32),
-  "race" VARCHAR(64),
-  "level" SMALLINT DEFAULT 1,
-  "alive" BOOLEAN DEFAULT TRUE,
-  "health_bars" JSONB DEFAULT '[]'::jsonb, -- [{"health": {"value": 20, "max": 20}}]
-  "stats" JSONB DEFAULT '{}'::jsonb, -- {"strength": 10, "speed": 10}
-
--- UNFINISHED
--- Skill 'affects' must map dynamically to other stats/health_bars/etc.
-
-
-"skills" JSONB DEFAULT '[]'::jsonb, -- [{"name": "Heal", "affects": {"health": {"value": "+10"}}}]
-
-  "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE "items" (
-  "id" UUID NOT NULL DEFAULT uuidv7() PRIMARY KEY,
-  "owner_user_id" UUID REFERENCES users(id) ON DELETE CASCADE,
-  "name" VARCHAR(128) NOT NULL,
-  "description" TEXT,
-  "stats" JSON DEFAULT '[]'::jsonb, -- [{"name": "Cost", "value": 2}, {"name": "Rarity", "value": "Mystical"}]
-  "damage" JSONB DEFAULT '{}'::jsonb, -- {"type": "slash", "flat": 0, "roll": "1d20"}
-
--- UNFINISHED
--- Skill 'affects' must map dynamically to other stats/health_bars/etc.
-"skills" JSONB DEFAULT '[]'::jsonb -- [{"name": "Heal", "affects": {"health": {"value": "+10"}}}]
-);
-
-CREATE TABLE "enemies" (
+CREATE TABLE campaign_session (
     "id" UUID NOT NULL DEFAULT uuidv7() PRIMARY KEY,
-    "name" VARCHAR(128) NOT NULL,
-    "description" TEXT
+    "campaign_id" UUID NOT NULL REFERENCES campaign(id),
+    "created_date" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    "start_date" TIMESTAMPTZ,
+    "story" TEXT
+);
+
+CREATE TABLE activity_log (
+    "id" UUID NOT NULL DEFAULT uuidv7() PRIMARY KEY,
+    "campaign_session_id" UUID NOT NULL REFERENCES campaign_session(id),
+    "created_date" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    "combat_turn" SMALLINT,
+    "log_header" TEXT,
+    "log" TEXT NOT NULL
+);
+
+CREATE TABLE character_template (
+    "id" UUID NOT NULL DEFAULT uuidv7() PRIMARY KEY,
+    "campaign_id" UUID NOT NULL REFERENCES campaign(id),
+    "created_date" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    "resource_pools" JSONB,
+    "attributes" JSONB
+);
+
+CREATE TABLE character (
+    "id" UUID NOT NULL DEFAULT uuidv7() PRIMARY KEY,
+    "user_account_id" UUID NOT NULL REFERENCES user_account(id),
+    "template_id" UUID NOT NULL REFERENCES user_account(id),
+    "created_date" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "level" SMALLINT NOT NULL DEFAULT 1,
+    "alive" BOOLEAN NOT NULL DEFAULT TRUE,
+    "race" TEXT,
+    "class" TEXT,
+    "resource_pools" HSTORE,
+    "attributes" HSTORE,
+    "image" TEXT
+);
+
+CREATE TABLE character_skill (
+    "id" UUID NOT NULL DEFAULT uuidv7() PRIMARY KEY,
+    "character_id" UUID NOT NULL REFERENCES character(id),
+    "created_date" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "roll" VARCHAR(18)
+);
+
+CREATE TABLE enemy (
+    "id" UUID NOT NULL DEFAULT uuidv7() PRIMARY KEY,
+    "campaign_id" UUID NOT NULL REFERENCES campaign(id),
+    "template_id" UUID NOT NULL REFERENCES character_template(id),
+    "created_date" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "resource_pools" HSTORE,
+    "attributes" HSTORE,
+    "image" TEXT
+);
+
+CREATE TABLE enemy_skill (
+    "id" UUID NOT NULL DEFAULT uuidv7() PRIMARY KEY,
+    "enemy_id" UUID NOT NULL REFERENCES enemy(id),
+    "created_date" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "roll" VARCHAR(18)
+);
+
+CREATE TABLE item (
+    "id" UUID NOT NULL DEFAULT uuidv7() PRIMARY KEY,
+    "character_id" UUID NOT NULL REFERENCES character(id),
+    "campaign_id" UUID NOT NULL REFERENCES campaign(id),
+    "created_date" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "roll" VARCHAR(18),
+    "image" TEXT
+);
+
+CREATE TABLE item_skill (
+    "id" UUID NOT NULL DEFAULT uuidv7() PRIMARY KEY,
+    "item_id" UUID NOT NULL REFERENCES item(id),
+    "created_date" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "roll" VARCHAR(18)
 );
