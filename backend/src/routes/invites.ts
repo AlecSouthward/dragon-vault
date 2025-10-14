3ba48f6ab19f617371627acd71ae96fa9c66cd5c
Alec Southward <alecsouthward@gmail.com>
2025-10-14 11:29:11 +0200

    Cleanup and overall improve consume invite route

--- backend/src/routes/invites.ts
+++ backend/src/routes/invites.ts
@@ -8,11 +8,11 @@ import { createUser } from '../utils/user';
 
 const inviteRoutes: FastifyPluginAsyncZod = async (app) => {
   app.post(
-    '/:id',
+    '/:inviteId',
     {
       schema: {
         params: z.object({
-          id: z.uuidv7(),
+          inviteId: z.uuidv7(),
         }),
         body: z.strictObject({
           username: z.string().nonoptional(),
@@ -21,70 +21,84 @@ const inviteRoutes: FastifyPluginAsyncZod = async (app) => {
       },
     },
     async (req, res) => {
-      const { id: inviteId } = req.params;
+      const { inviteId } = req.params;
       const { username, password } = req.body;
 
+      const invite = await app.db
+        .selectFrom('userInvite')
+        .select(['createdDate', 'usedByUserAccountId'])
+        .where('id', '=', inviteId)
+        .executeTakeFirst();
+
+      if (!invite) {
+        app.log.error({ inviteId, username }, 'Failed to find invite by id');
+
+        return res
+          .code(StatusCodes.NOT_FOUND)
+          .send({ message: 'No invite was found with that id' });
+      }
+
+      const expirationTime =
+        invite.createdDate.getTime() + INVITE_EXPIRE_OFFSET_MS;
+      const currentTime = Date.now();
+
+      if (currentTime > expirationTime) {
+        app.log.error(
+          { inviteId, username },
+          'Failed to use invite as it has expired'
+        );
+
+        return res
+          .code(StatusCodes.GONE)
+          .send({ message: 'The invite has expired' });
+      } else if (invite.usedByUserAccountId) {
+        app.log.error(
+          {
+            inviteId,
+            usedByUserAccountId: invite.usedByUserAccountId,
+            username,
+          },
+          'Failed to use invite as it has already been used'
+        );
+
+        return res
+          .code(StatusCodes.GONE)
+          .send({ message: 'The invite has already been used' });
+      }
+
+      const createUserResponse = await createUser(username, password);
+
+      if (!createUserResponse.ok || !createUserResponse.newUserId) {
+        app.log.error(
+          { err: createUserResponse.error, inviteId, username },
+          'An error occurred when creating a user from an invite'
+        );
+
+        return res
+          .code(createUserResponse.httpCode)
+          .send({ message: createUserResponse.error });
+      }
+
       try {
-        const invites = await app.db
-          .selectFrom('userInvite')
-          .select(['createdDate', 'usedByUserAccountId'])
-          .where('id', '=', inviteId)
-          .execute();
-
-        if (invites.length === 0) {
-          return res
-            .code(StatusCodes.NOT_FOUND)
-            .send({ message: 'No invite was found with that id' });
-        } else if (invites.length > 1) {
-          app.log.error(
-            { username, inviteId },
-            'Multiple invites were returned from a single id, using the oldest one'
-          );
-        }
-
-        const inviteToUse = invites[0];
-        const expirationTime =
-          inviteToUse.createdDate.getTime() + INVITE_EXPIRE_OFFSET_MS;
-        const currentTime = Date.now();
-
-        if (currentTime > expirationTime) {
-          return res
-            .code(StatusCodes.GONE)
-            .send({ message: 'The invite has expired' });
-        } else if (inviteToUse.usedByUserAccountId) {
-          return res
-            .code(StatusCodes.GONE)
-            .send({ message: 'The invite has already been used' });
-        }
-
-        const createUserResponse = await createUser(username, password);
-
-        if (!createUserResponse.ok || !createUserResponse.newUserId) {
-          app.log.error(
-            { err: createUserResponse.error },
-            'An error occurred when creating a user from an invite'
-          );
-          return res
-            .code(createUserResponse.httpCode)
-            .send({ message: createUserResponse.error });
-        }
-
         await app.db
           .updateTable('userInvite')
           .set('usedByUserAccountId', createUserResponse.newUserId)
           .where('id', '=', inviteId)
           .execute();
-
-        return res
-          .code(createUserResponse.httpCode)
-          .send({ message: createUserResponse.message });
       } catch (err) {
-        app.log.error(err, 'An error occurred when using the invite');
+        app.log.error(
+          { err, inviteId, username },
+          'An error occurred when using an invite'
+        );
 
         return res
           .code(StatusCodes.INTERNAL_SERVER_ERROR)
           .send({ message: 'Failed to use the invite' });
       }
+
+      return res
+        .code(createUserResponse.httpCode)
+        .send({ message: createUserResponse.message });
     }
   );
 };
